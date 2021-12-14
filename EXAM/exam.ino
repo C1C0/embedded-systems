3934ff3642ca3c64afa5ac7b3a88fafe02abc9d8
C1C0 <kpskola1002@gmail.com>
2021-12-14 10:02:54 +0100

    DHT11 Digital temp. sensor

--- EXAM/exam.ino
+++ EXAM/exam.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include "DHT.h"
 
 /**
  * @brief from I2C hello world example
@@ -32,16 +33,40 @@ typedef struct TEMP_METER {
   unsigned long lastCheckTimeMS;
 } TEMP_METER;
 
-TEMP_METER lm35 = {A0, 0, 500, 0};
+typedef struct DHT_S {
+  /**
+   * @brief expected delay between measurements
+   *
+   */
+  int delay;
+  float value;
+  unsigned long lastMeasurement;
+  unsigned long lastReading;
+} DHT_S;
+
+TEMP_METER lm35 = {A0, 0, 2000, 0};
+
+#define DHTPIN 7
+#define DHTTYPE DHT11 
+DHT dht(DHTPIN, DHTTYPE);
+DHT_S dhtS = {2000, 0};
 
 void setup() {
   Serial.begin(9600);
 
   initLCD(&lcd);
+
   printToLCD(&lcd, 3, 2, "Hello world 1");
+
+  dht.begin();
+
+  delay(2000);
 }
 
-void loop() { delayedTempCheck(&lcd, &lm35); }
+void loop() {
+  delayedTempCheck(&lcd, &lm35);
+  checkTempDigital(&lcd, &dht, &dhtS);
+}
 
 /**
  * @brief Setup for specified LCD display
@@ -79,10 +104,10 @@ void delayedTempCheck(LiquidCrystal_I2C *lcd, TEMP_METER *temp) {
 
     temp->lastCheckTimeMS = millis();
 
-    checkTemp(temp);
+    checkTempAnalog(temp);
 
     // Convert float value into string
-    dtostrf(temp->value, -3, 1, tempS + 2);
+    dtostrf(temp->value, -3, 1, tempS + 3);
 
     // Append C
     strcat(tempS, "C");
@@ -96,9 +121,32 @@ void delayedTempCheck(LiquidCrystal_I2C *lcd, TEMP_METER *temp) {
  *
  * @param temp
  */
-void checkTemp(TEMP_METER *temp) {
+void checkTempAnalog(TEMP_METER *temp) {
   // read the value from the PIN,
   // convert this value to voltage (5000 / 1024.0),
   // and then to temperature (/10)
   temp->value = analogRead(lm35.pin) * (5000 / 1024.0F) / 10;
+}
+
+void checkTempDigital(LiquidCrystal_I2C *lcd, DHT *dht, DHT_S *dht_s) {
+  if (millis() - dht_s->lastMeasurement > dht_s->delay) {
+    char tempS[11] = "D7:";
+
+    dht_s->lastMeasurement = millis();
+
+    if (millis() - dht_s->lastReading > 300) {
+      dht_s->value = dht->readTemperature();
+      dht_s->lastReading = millis();
+    }
+
+    Serial.println(dht_s->value);
+
+    // Convert float value into string
+    dtostrf(dht_s->value, -3, 1, tempS + 3);
+
+    // Append C
+    strcat(tempS, "C");
+
+    printToLCD(lcd, 0, 1, tempS);
+  }
 }
