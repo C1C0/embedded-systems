784ed2c86385cb1ed5d8fc60f75bd3ce3178a0e9
C1C0 <kpskola1002@gmail.com>
2021-12-14 09:30:08 +0100

    LM35 Analog temperature

--- EXAM/exam.ino
+++ EXAM/exam.ino
@@ -1,9 +1,38 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+/**
+ * @brief from I2C hello world example
+ *
+ */
 LiquidCrystal_I2C
     lcd(0x27, 20,
         4); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+/**
+ * @brief Temperature meter
+ */
+typedef struct TEMP_METER {
+  byte pin : 4;
+
+  /**
+   * @brief last measured value should be stored here
+   */
+  float value;
+
+  /**
+   * @brief How often to check the temperature of specified meter
+   */
+  unsigned int checkTimeMs;
+
+  /**
+   * @brief Preferebly used to assigned millis() function value
+   *        for further comparison
+   */
+  unsigned long lastCheckTimeMS;
+} TEMP_METER;
+
+TEMP_METER lm35 = {A0, 0, 500, 0};
 
 void setup() {
   Serial.begin(9600);
@@ -12,7 +41,7 @@ void setup() {
   printToLCD(&lcd, 3, 2, "Hello world 1");
 }
 
-void loop() { Serial.println("TEst works"); }
+void loop() { delayedTempCheck(&lcd, &lm35); }
 
 /**
  * @brief Setup for specified LCD display
@@ -26,13 +55,50 @@ void initLCD(LiquidCrystal_I2C *lcd) {
 
 /**
  * @brief print to LCD
- * 
- * @param lcd 
- * @param col 
- * @param row 
- * @param message 
+ *
+ * @param lcd
+ * @param col
+ * @param row
+ * @param message
  */
 void printToLCD(LiquidCrystal_I2C *lcd, int col, int row, char *message) {
   lcd->setCursor(col, row);
   lcd->print(message);
+}
+
+/**
+ * @brief Delays the checking and re-printing of message
+ *
+ * @param lcd
+ * @param temp
+ */
+void delayedTempCheck(LiquidCrystal_I2C *lcd, TEMP_METER *temp) {
+  if ((millis() - temp->lastCheckTimeMS) > temp->checkTimeMs) {
+
+    char tempS[11] = "A0:";
+
+    temp->lastCheckTimeMS = millis();
+
+    checkTemp(temp);
+
+    // Convert float value into string
+    dtostrf(temp->value, -3, 1, tempS + 2);
+
+    // Append C
+    strcat(tempS, "C");
+
+    printToLCD(lcd, 0, 0, tempS);
+  }
+}
+
+/**
+ * @brief Get the Temperature of temp. meter
+ *
+ * @param temp
+ */
+void checkTemp(TEMP_METER *temp) {
+  // read the value from the PIN,
+  // convert this value to voltage (5000 / 1024.0),
+  // and then to temperature (/10)
+  temp->value = analogRead(lm35.pin) * (5000 / 1024.0F) / 10;
 }
